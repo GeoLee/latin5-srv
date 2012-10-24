68e296fcac561e6cecbe9099bc595ff47743cc3d
georgezeng <feleio@qq.com>
2012-10-24 11:08:24 +0800

    d

--- Latin5-srv/src/com/latin5/dao/impl/BaseDAOImpl.java
+++ Latin5-srv/src/com/latin5/dao/impl/BaseDAOImpl.java
@@ -1,17 +1,14 @@
 
 
 package com.latin5.dao.impl;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.dao.support.DaoSupport;
-import org.springframework.orm.ibatis.SqlMapClientCallback;
 import org.springframework.orm.ibatis.SqlMapClientTemplate;
 import org.springframework.util.Assert;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
-import com.ibatis.sqlmap.client.SqlMapExecutor;
 import com.latin5.dao.BaseDAO;
 
 public class BaseDAOImpl extends DaoSupport implements BaseDAO{
@@ -66,19 +63,6 @@ public class BaseDAOImpl extends DaoSupport implements BaseDAO{
 	public boolean delete(String statementName,Map<String ,Object> paramMap)throws Exception{
 		return slaveSqlMapClientTemplate.delete(statementName, paramMap)>0;
 	}
-	@SuppressWarnings("unchecked")
-	public void batchInsert(final String statementName,final List dataList)throws Exception{
-		this.slaveSqlMapClientTemplate.execute(new SqlMapClientCallback() { 
-            public Object doInSqlMapClient(SqlMapExecutor executor) throws SQLException { 
-                executor.startBatch(); 
-                for (int i = 0; i < dataList.size(); i++) { 
-                	Object object = (Object) dataList.get(i); 
-                    executor.insert(statementName, object); 
-                } 
-                executor.executeBatch(); 
-                return null; 
-            } 
-        }); 
-	}
+	
 }
 
