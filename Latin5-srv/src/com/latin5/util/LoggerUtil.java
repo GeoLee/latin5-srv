a15c5ed12168bfac3bf82fea675e20e07c8abb46
georgezeng <feleio@qq.com>
2012-10-24 15:03:26 +0800

    a

--- Latin5-srv/src/com/latin5/util/LoggerUtil.java
+++ Latin5-srv/src/com/latin5/util/LoggerUtil.java
@@ -4,31 +4,22 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class LoggerUtil {
-
+	
 	/**
-	 * sms的错误日志打印
+	 * 普通日志
 	 * @param msg
 	 * @param e
 	 */
-	public static void logSmsError(String msg,Exception e){
-		logError(LoggerModuleType.MODULE_NAME_SMS_API, msg, e);
+	public static void logError(String msg,Exception e){
+		Log log=LogFactory.getLog("");
+		log.error(msg,e);
 	}
-	/**
-	 * active mq.错误日志打印。
-	 * @param msg
-	 * @param e
-	 */
-	public static void logActiveMqError(String msg,Exception e){
-		logError(LoggerModuleType.MODULE_NAME_ACTIVE_MQ_API, msg, e);
+	public static void logInfo(String moduleName,String msg){
+		Log log=LogFactory.getLog(moduleName);
+		log.info(msg);
 	}
-	/**
-	 * email发送日志。
-	 * @param msg
-	 * @param e
-	 */
-	public static void logEmailError(String msg,Exception e){
-		logError(LoggerModuleType.MODULE_NAME_EMAIL, msg, e);
-	}
+	
+	
 	/**
 	 * fetch log
 	 * @param msg
@@ -41,16 +32,8 @@ public class LoggerUtil {
 		logInfo(LoggerModuleType.MODULE_NAME_FETCH_API, msg);
 	}
 	
-	
-	
-	
-	public static void logError(String msg,Exception e){
-		Log log=LogFactory.getLog("");
-		log.error(msg,e);
-	}
-	
 	/**
-	 * 针对邮件、53kf将采取独立目录下记录。目前支持mail,53kf、电话接口
+	 * 独立模块日志
 	 * @param moduleName
 	 * @param msg
 	 */
@@ -62,15 +45,8 @@ public class LoggerUtil {
 			log.error(msg);
 		}
 	}
-	public static void logInfo(String moduleName,String msg){
-		Log log=LogFactory.getLog(moduleName);
-		log.info(msg);
-	}
 	
 	public static class LoggerModuleType{
-		public static String MODULE_NAME_EMAIL="email";
-		public static String MODULE_NAME_ACTIVE_MQ_API="activeMqApi";
-		public static String MODULE_NAME_SMS_API="smsApi";
 		public static String MODULE_NAME_FETCH_API="fetchApi";
 	}
 }
